db03bd49cb49171fee0af16d41fcac4f10dfc968
arda demirtas <87753956+arda-demirtas@users.noreply.github.com>
2025-05-24 01:24:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,28 @@ def get_db():
 @app.get("/")
 def dddd():
     return {"message" : "basarili2"}
+
+/*
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.create_user(db, user)
+    return db_user
+*/
+
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # Kullanıcı adı kontrolü
+    existing_user = db.query(models.User).filter(
+        (models.User.username == user.username) | (models.User.email == user.email)
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Kullanıcı adı veya email zaten kayıtlı."
+        )
+
     db_user = crud.create_user(db, user)
     return db_user
 
